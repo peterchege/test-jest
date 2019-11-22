d2c1acca771f837d698b12a27245285b3e4075de
peterkinuthia <peterchege442@gmail.com>
2019-11-22 09:18:54 +0300

    correct test case

--- sketch.test.js
+++ sketch.test.js
@@ -1,9 +1,9 @@
 const sum = require('./sketch');
 // const diff = require('./sketch');
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-});
+// test('adds 1 + 2 to equal 3', () => {
+//     expect(sum(1, 2)).toBe(3);
+// });
 
 test('adds 1 + 2 to equal 3', sumTest);
 
